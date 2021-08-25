c9124e562dfa17ab6a29cc2ee48cbd1dd7dc0339
KarinaAndros <84373468+KarinaAndros@users.noreply.github.com>
2021-08-25 08:43:57 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 150) {
+            $('#text_block').fadeOut();
+            $('.camry_img').width(1300);
+            $('.camry_img').height(700);
+            $('.camry_img').css('backgroundImage', 'url(img/camry3.png)');
+            $('.feature').fadeIn();
+            $('.title').fadeIn();
+            $('.conditions').css('display', 'flex');
+        }
+
+    });
+});
